ef7d80b56e565a1e269e7d5dd18f82ed7c265355
hvkst <bernd@hvkst.de>
2022-12-10 13:20:35 +0100

    auth working, reload possible

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { BASE_URL } from '../hooks/config';
 
 export const UserContext = createContext();
 
@@ -8,6 +9,31 @@ export function UserContextProvider({ children }) {
   const logoutUser = () => {
     setUser(null);
   };
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const url = BASE_URL + '/auth/verify';
+        console.log('url:', url);
+
+        const res = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+        const data = await res.json();
+        console.log('data.user:', data.user);
+
+        setUser(data.user);
+        console.log('Found User (from Context)');
+      } catch (err) {
+        console.error('UserContext', err);
+      }
+    };
+    getUser();
+  }, []);
 
   return <UserContext.Provider value={{ user, loginUser: setUser, logoutUser }}>{children}</UserContext.Provider>;
 }
