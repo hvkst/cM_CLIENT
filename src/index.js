e5b351cd5bceca559cdf310fd776e0e1d1aaff44
hvkst <bernd@hvkst.de>
2023-01-12 23:27:05 +0100

    fixing env

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { UserContextProvider } from './context/UserContext';
 import { ProjectContextProvider } from './context/ProjectContext';
 import { AllUserContextProvider } from './context/AllUserContext';
 import { ErrorContextProvider } from './context/ErrorContext';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
