e325e5e2de3f74d3473323152401a7a940654944
hvkst <bernd@hvkst.de>
2023-01-13 00:36:29 +0100

    deploying

--- src/components/Main/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/Main/ErrorBoundary/ErrorBoundary.jsx
@@ -1,23 +1,23 @@
 import { useRouteError } from 'react-router-dom';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 export default function ErrorBoundary() {
   let error = useRouteError();
   console.error(error);
   return (
-    <Wrapper>
+    <div>
       <h3>{error.status}</h3>
       <h3>{error.statusText}</h3>
-    </Wrapper>
+    </div>
   );
 }
 
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 50vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background-color: lightpink;
-`;
+// const Wrapper = styled.div`
+//   width: 100vw;
+//   height: 50vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   background-color: lightpink;
+// `;
